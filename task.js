bd28d4279090896ef5fb980b6d82d89fe7fec546
LexusIgnatenko <133475786+LexusIgnatenko@users.noreply.github.com>
2025-06-18 09:56:13 +0300

    Add files via upload

---
+++ task.js
@@ -0,0 +1,18 @@
+// Начальное значение таймера
+let remainingSeconds = 59;
+const timerElement = document.querySelector('#timer'); // Элемент для отображения таймера
+
+// Отображаем текущее значение таймера
+timerElement.textContent = remainingSeconds;
+
+// Запускаем таймер
+const countdownInterval = setInterval(function () {
+    remainingSeconds--; // Уменьшаем оставшееся время на одну секунду
+    timerElement.textContent = remainingSeconds; // Обновляем экран
+
+    // Если таймер дошёл до нуля
+    if (remainingSeconds <= 0) {
+        clearInterval(countdownInterval); // Останавливаем таймер
+        alert('Вы победили в конкурсе!');
+    }
+}, 100); // Интервал срабатывания — раз в секунду
